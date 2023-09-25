73196720770c33d19eb4f7a9ec87235abae847b7
KhangDuong <khangdt10@fpt.com.vn>
2023-09-25 09:49:22 +0700

    feat(cache): cache pkg

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -7,6 +7,10 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+func init() {
+	NewCache()
+}
+
 type Cache interface {
 	Get(key string) (string, error)
 	Set(key string, value string, expiration time.Duration) error
